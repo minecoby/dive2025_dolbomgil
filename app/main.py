ad3ca81469dad1151d5732f85793a055e3c6a704
minecoby <minecoby@naver.com>
2025-08-20 01:18:01 +0900

    포트변경

--- app/main.py
+++ app/main.py
@@ -37,4 +37,4 @@ app.include_router(safe_zone_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=7777, reload=True)
